dd3c55ad25335e42934cb4305261d2bdb688f01b
gad-biton <gadb633@gmail.com>
2026-07-07 11:14:12 +0300

    added View the board

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 bord_tick_tack_tow = ["","","","","","","","",""]
+print(f"  {bord_tick_tack_tow[0]} | {bord_tick_tack_tow[1]} | {bord_tick_tack_tow[2]}  \n-----------\n  {bord_tick_tack_tow[3]} | {bord_tick_tack_tow[4]} | {bord_tick_tack_tow[5]}  \n-----------\n  {bord_tick_tack_tow[6]} | {bord_tick_tack_tow[7]} | {bord_tick_tack_tow[8]}")
