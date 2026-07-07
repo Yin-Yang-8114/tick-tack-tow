8677f6e174fb078442c219386ab11d092d21155a
Israel Meir <i0509711452@gmail.com>
2026-07-07 15:36:11 +0300

    check_row

--- main.py
+++ main.py
@@ -24,3 +24,4 @@ for i in range(9):
 
     print(
         f"  {bord[0]} | {bord[1]} | {bord[2]}  \n-----------\n  {bord[3]} | {bord[4]} | {bord[5]}  \n-----------\n  {bord[6]} | {bord[7]} | {bord[8]}")
+
---
+++ win_check.py
@@ -0,0 +1,12 @@
+
+def check_row():
+    if corrent_user == bord [0] and corrent_user == bord[1] and corrent_user == bord[2]:
+        return f" {corrent_name} he is the winner."
+    if corrent_user == bord[3] and corrent_user == bord[4] and corrent_user == bord [5]:
+        return f"{corrent_name} he is the winner."
+    if corrent_user == bord [6]and corrent_user == bord [7]and corrent_user == bord[8]:
+        return f"{corrent_name} he is the winner."
+    else:
+        return
+print(check_row())
+
