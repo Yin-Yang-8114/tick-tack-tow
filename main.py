628140d1463fecc2561dd45c9b90d8283f0772c5
Yin Yang <mihiparis@gmail.com>
2026-07-07 14:22:18 +0300

    added range check and added drow option after all squres are taken

--- main.py
+++ main.py
@@ -9,18 +9,27 @@ corrent_user = sing_1
 corrent_name = user_1
 
 bord = ["","","","","","","","",""]
-
-for i in range(9):
-    choice = int(input(f'hi {corrent_name} please choose your please 0-8 '))
-    if corrent_user == sing_1:
-        bord[choice] = corrent_user
-        corrent_name = user_2
-        corrent_user = sing_2
+game_running = True
+time_of_run = 0
+while game_running:
+    if time_of_run >= 9:
+        game_running = False
+        print("drow")
     else:
-        bord[choice] = corrent_user
-        corrent_user = sing_1
-        corrent_name = user_1
-
-
-    print(
-        f"  {bord[0]} | {bord[1]} | {bord[2]}  \n-----------\n  {bord[3]} | {bord[4]} | {bord[5]}  \n-----------\n  {bord[6]} | {bord[7]} | {bord[8]}")
+        choice = int(input(f'hi {corrent_name} please choose your please 0-8 '))
+        if bord[choice] != "":
+            print("this number is already taken")
+        else:
+            bord[choice] = corrent_user
+            if corrent_user == sing_1:
+                bord[choice] = corrent_user
+                corrent_name = user_2
+                corrent_user = sing_2
+                time_of_run +=1
+            else:
+                bord[choice] = corrent_user
+                corrent_user = sing_1
+                corrent_name = user_1
+                time_of_run += 1
+            print(
+                f"  {bord[0]} | {bord[1]} | {bord[2]}  \n-----------\n  {bord[3]} | {bord[4]} | {bord[5]}  \n-----------\n  {bord[6]} | {bord[7]} | {bord[8]}")
