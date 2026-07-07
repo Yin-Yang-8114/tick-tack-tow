c580ede70b45a4f5870e3cca779c7a82663d26da
gad-biton <gadb633@gmail.com>
2026-07-07 15:53:14 +0300

    fixed all fu agene 2

--- win_check.py
+++ win_check.py
@@ -17,3 +17,13 @@ def Diagonal_check():
     
 def main_check():
     pass    
+
+def check_row():
+    if corrent_user == bord [0] and corrent_user == bord[1] and corrent_user == bord[2]:
+        return f" {corrent_name} he is the winner."
+    if corrent_user == bord[3] and corrent_user == bord[4] and corrent_user == bord [5]:
+        return f"{corrent_name} he is the winner."
+    if corrent_user == bord [6]and corrent_user == bord [7]and corrent_user == bord[8]:
+        return f"{corrent_name} he is the winner."
+    else:
+        return
